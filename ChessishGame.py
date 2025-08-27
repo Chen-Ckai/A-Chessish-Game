b5389b74adcbfffc82e921585c3b77590362b9b2
Ckai <chenckai477@gmail.com>
2025-08-27 18:17:07 -0400

    I added the tutorial text

---
+++ ChessishGame.py
@@ -0,0 +1,76 @@
+board = [
+
+    [
+    # Row 1
+    {"name": "cell-1-1", "type": "start", "north": None,       "south": "cell-2-1", "east": "cell-1-2", "west": None},
+    {"name": "cell-1-2", "type": "blank", "north": None,       "south": "cell-2-2", "east": "cell-1-3", "west": "cell-1-1"},
+    {"name": "cell-1-3", "type": "blank", "north": None,       "south": "cell-2-3", "east": "cell-1-4", "west": "cell-1-2"},
+    {"name": "cell-1-4", "type": "blank", "north": None,       "south": "cell-2-4", "east": "cell-1-5", "west": "cell-1-3"},
+    {"name": "cell-1-5", "type": "blank", "north": None,       "south": "cell-2-5", "east": "cell-1-6", "west": "cell-1-4"},
+    {"name": "cell-1-6", "type": "blank", "north": None,       "south": "cell-2-6", "east": None,       "west": "cell-1-5"},
+    ],
+
+    # Row 2
+    [
+    {"name": "cell-2-1", "type": "blank", "north": "cell-1-1", "south": "cell-3-1", "east": "cell-2-2", "west": None},
+    {"name": "cell-2-2", "type": "blank", "north": "cell-1-2", "south": "cell-3-2", "east": "cell-2-3", "west": "cell-2-1"},
+    {"name": "cell-2-3", "type": "spike", "north": "cell-1-3", "south": "cell-3-3", "east": "cell-2-4", "west": "cell-2-2"},
+    {"name": "cell-2-4", "type": "button", "north": "cell-1-4", "south": "cell-3-4", "east": "cell-2-5", "west": "cell-2-3"},
+    {"name": "cell-2-5", "type": "blank", "north": "cell-1-5", "south": "cell-3-5", "east": "cell-2-6", "west": "cell-2-4"},
+    {"name": "cell-2-6", "type": "blank", "north": "cell-1-6", "south": "cell-3-6", "east": None,       "west": "cell-2-5"},
+    ],
+    [
+    # Row 3
+    {"name": "cell-3-1", "type": "button", "north": "cell-2-1", "south": "cell-4-1", "east": "cell-3-2", "west": None},
+    {"name": "cell-3-2", "type": "blank", "north": "cell-2-2", "south": "cell-4-2", "east": "cell-3-3", "west": "cell-3-1"},
+    {"name": "cell-3-3", "type": "blank", "north": "cell-2-3", "south": "cell-4-3", "east": "cell-3-4", "west": "cell-3-2"},
+    {"name": "cell-3-4", "type": "blank", "north": "cell-2-4", "south": "cell-4-4", "east": "cell-3-5", "west": "cell-3-3"},
+    {"name": "cell-3-5", "type": "blank", "north": "cell-2-5", "south": "cell-4-5", "east": "cell-3-6", "west": "cell-3-4"},
+    {"name": "cell-3-6", "type": "blank", "north": "cell-2-6", "south": "cell-4-6", "east": None,       "west": "cell-3-5"},
+    ],
+    [
+    # Row 4
+    {"name": "cell-4-1", "type": "blank", "north": "cell-3-1", "south": "cell-5-1", "east": "cell-4-2", "west": None},
+    {"name": "cell-4-2", "type": "blank", "north": "cell-3-2", "south": "cell-5-2", "east": "cell-4-3", "west": "cell-4-1"},
+    {"name": "cell-4-3", "type": "blank", "north": "cell-3-3", "south": "cell-5-3", "east": "cell-4-4", "west": "cell-4-2"},
+    {"name": "cell-4-4", "type": "blank", "north": "cell-3-4", "south": "cell-5-4", "east": "cell-4-5", "west": "cell-4-3"},
+    {"name": "cell-4-5", "type": "blank", "north": "cell-3-5", "south": "cell-5-5", "east": "cell-4-6", "west": "cell-4-4"},
+    {"name": "cell-4-6", "type": "blank", "north": "cell-3-6", "south": "cell-5-6", "east": None,       "west": "cell-4-5"},
+    ],
+    [
+    # Row 5
+    {"name": "cell-5-1", "type": "blank", "north": "cell-4-1", "south": "cell-6-1", "east": "cell-5-2", "west": None},
+    {"name": "cell-5-2", "type": "wall", "north": "cell-4-2", "south": "cell-6-2", "east": "cell-5-3", "west": "cell-5-1"},
+    {"name": "cell-5-3", "type": "blank", "north": "cell-4-3", "south": "cell-6-3", "east": "cell-5-4", "west": "cell-5-2"},
+    {"name": "cell-5-4", "type": "blank", "north": "cell-4-4", "south": "cell-6-4", "east": "cell-5-5", "west": "cell-5-3"},
+    {"name": "cell-5-5", "type": "blank", "north": "cell-4-5", "south": "cell-6-5", "east": "cell-5-6", "west": "cell-5-4"},
+    {"name": "cell-5-6", "type": "button", "north": "cell-4-6", "south": "cell-6-6", "east": None,       "west": "cell-5-5"},
+    ],
+    [
+    # Row 6
+    {"name": "cell-6-1", "type": "blank", "north": "cell-5-1", "south": None,       "east": "cell-6-2", "west": None},
+    {"name": "cell-6-2", "type": "button", "north": "cell-5-2", "south": None,       "east": "cell-6-3", "west": "cell-6-1"},
+    {"name": "cell-6-3", "type": "blank", "north": "cell-5-3", "south": None,       "east": "cell-6-4", "west": "cell-6-2"},
+    {"name": "cell-6-4", "type": "blank", "north": "cell-5-4", "south": None,       "east": "cell-6-5", "west": "cell-6-3"},
+    {"name": "cell-6-5", "type": "blank", "north": "cell-5-5", "south": None,       "east": "cell-6-6", "west": "cell-6-4"},
+    {"name": "cell-6-6", "type": "blank", "north": "cell-5-6", "south": None,       "east": None,       "west": "cell-6-5"}
+    ]
+]
+
+
+def print_board_grid(board):
+    for r in range(6):
+        for c in range(6):
+            cell = board[r][c]
+            print(f"{cell['name']} ({cell['type']})", end=" | ")
+        print("\n" + "-" * 120)
+
+# Example usage
+print_board_grid(board)
+
+while True:
+    print("Welcome to the tutorial for A Chessish Game, a puzzle game with different objectives and obstacles. \n This game has you play as a chess piece, and currently, you are a queen (you can move any number of spaces in all eight directions). \n You may notice that certain spaces on the board have different names. \n The button spaces are your win condition: you must stand on each to win. \n The spike space will make you lose when stepped on. \n The wall space will stop your movement, and cannot be stepped on. Got that? \n Ok go wander into the wild good luck")
+    command = input("type 'exit' to stop: ")
+    if command.lower() == "exit":
+        break
+    print(f"You typed: {command}")
